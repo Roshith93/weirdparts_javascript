dc1aafeee500e9ccae865947cb5fa70124572984
Roshith R <r.roshith@gmail.com>
2020-09-03 12:45:00 +0530

    checking with functions

--- strictMode.js
+++ strictMode.js
@@ -1,3 +1,10 @@
-'use strict';
-person = {};
+// 'use strict';
+person = { firstName: '2' };
 console.log(person);
+
+function Hi() {
+	'use strict';
+	person1 = {};
+	console.log(person1);
+}
+Hi();
